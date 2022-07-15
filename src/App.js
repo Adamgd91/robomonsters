254deb465784ab9788495ccbc3ec087e0e701111
Adam Davidson <agdavidson91@gmail.com>
2022-07-15 12:39:47 -0400

    added button and setState for name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Robo Monster</h1>
-    </div>
-  );
-}
+import { Component } from "react";
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Monster",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Robo {this.state.name}</h1>
+        <button
+          onClick={() => {
+            this.setState({ name: "chicken" });
+          }}
+        >
+          Change Name
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
